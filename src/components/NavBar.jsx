1ccae3bd2dcf89e05e6b55a3ef060fb521f42e1d
eva-hs <eva.holmback@gmail.com>
2022-07-23 15:05:54 +0200

    Changed Navbar to a stateless function

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,29 +1,23 @@
-import React, { Component } from "react";
+import React from "react";
 
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <div className="container-fluid">
-          <span className="navbar-brand h1">
-            Navbar
-            {/* Badge will only show amount of products with values >0 */}
-            <span>{this.formatProductCountBadge(this.props.productCount)}</span>
+function NavBar(productCount) {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container-fluid">
+        <span className="navbar-brand h1">
+          Navbar
+          {/* Badge will only show amount of products with values >0 */}
+          <span>
+            {productCount > 0 && (
+              <span className="badge rounded-pill bg-secondary ms-2">
+                {productCount}
+              </span>
+            )}
           </span>
-        </div>
-      </nav>
-    );
-  }
-
-  formatProductCountBadge(productCount) {
-    return (
-      productCount > 0 && (
-        <span className="badge rounded-pill bg-secondary ms-2">
-          {productCount}
         </span>
-      )
-    );
-  }
+      </div>
+    </nav>
+  );
 }
 
 export default NavBar;
