e39007383a5e0cfe3d5eeba946c63b93236f7fd0
eva-hs <eva.holmback@gmail.com>
2022-07-23 15:30:49 +0200

    Adjusted productCount in Navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,18 +1,14 @@
 import React from "react";
 
-function NavBar(productCount) {
+function NavBar({ productCount }) {
   return (
     <nav className="navbar navbar-light bg-light">
       <div className="container-fluid">
         <span className="navbar-brand h1">
           Navbar
           {/* Badge will only show amount of products with values >0 */}
-          <span>
-            {productCount > 0 && (
-              <span className="badge rounded-pill bg-secondary ms-2">
-                {productCount}
-              </span>
-            )}
+          <span className="badge rounded-pill bg-secondary ms-2">
+            {productCount}
           </span>
         </span>
       </div>
